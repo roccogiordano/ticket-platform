25f746b2685ca4d47a4abb249e2db27aa3ab73e0
roccogiordano <rocco.giordano573@gmail.com>
2025-03-25 12:25:01 +0100

    feat: added controller, repository entity and index template for tickets

---
+++ ticket-platform/src/main/java/org/lessons/java/ticket_platform/controller/TicketController.java
@@ -0,0 +1,27 @@
+package org.lessons.java.ticket_platform.controller;
+
+import java.util.List;
+
+import org.lessons.java.ticket_platform.model.Ticket;
+import org.lessons.java.ticket_platform.repository.TicketRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/tickets")
+public class TicketController {
+    
+    @Autowired
+    private TicketRepository ticketRepository;
+
+    @GetMapping
+    public String view(Model model) {
+        List<Ticket> tickets = ticketRepository.findAll();
+        model.addAttribute(tickets);
+        return("/tickets/index");
+    }
+
+}
---
+++ ticket-platform/src/main/java/org/lessons/java/ticket_platform/model/Ticket.java
@@ -0,0 +1,23 @@
+package org.lessons.java.ticket_platform.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+
+@Entity
+@Table(name = "tickets")
+public class Ticket {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "name")
+    @NotBlank(message = "Name is mandatory")
+    private String name;
+    
+}
