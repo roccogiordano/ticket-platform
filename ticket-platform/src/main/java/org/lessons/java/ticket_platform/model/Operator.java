39bff6d4a45b3d2e42d161159a0d1f785a79b97a
roccogiordano <rocco.giordano573@gmail.com>
2025-03-25 14:44:26 +0100

    feat: added Operator entity and relation to Ticket

---
+++ ticket-platform/src/main/java/org/lessons/java/ticket_platform/model/Operator.java
@@ -0,0 +1,83 @@
+package org.lessons.java.ticket_platform.model;
+
+import java.util.List;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+
+@Entity
+@Table(name = "operators")
+public class Operator {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "first_name")
+    @NotBlank(message = "First name is mandatory")
+    private String firstName;
+
+    @Column(name = "last_name")
+    @NotBlank(message = "Last name is mandatory")
+    private String lastName;
+
+    @Column(name = "status")
+    @NotBlank(message = "Status is mandatory")
+    private String status;
+
+    @OneToMany(mappedBy = "operator")
+    private List<Ticket> tickets;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public List<Ticket> getTickets() {
+        return tickets;
+    }
+
+    public void setTickets(List<Ticket> tickets) {
+        this.tickets = tickets;
+    }
+
+    @Override
+    public String toString() {
+        return "Operator [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", status=" + status
+                + "]";
+    }
+
+}
--- ticket-platform/src/main/java/org/lessons/java/ticket_platform/model/Ticket.java
+++ ticket-platform/src/main/java/org/lessons/java/ticket_platform/model/Ticket.java
@@ -1,12 +1,18 @@
 package org.lessons.java.ticket_platform.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.annotation.Nullable;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 @Entity
 @Table(name = "tickets")
@@ -19,5 +25,70 @@ public class Ticket {
     @Column(name = "name")
     @NotBlank(message = "Name is mandatory")
     private String name;
-    
+
+    @Column(name = "status")
+    @NotBlank(message = "Status is mandatory")
+    private String status;
+
+    @Column(name = "notes")
+    @Nullable
+    private String notes;
+
+    @ManyToOne
+    @JoinColumn(name = "operator_id", nullable = false)
+    @NotNull(message = "Operator ID is mandatory")
+    @JsonIgnore
+    private Operator operator;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getNotes() {
+        return notes;
+    }
+
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
+
+    public Operator getOperator() {
+        return operator;
+    }
+
+    public void setOperator(Operator operator) {
+        this.operator = operator;
+    }
+
+    @Override
+    public String toString() {
+        return "Ticket{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", status='" + status + '\'' +
+                ", notes='" + notes + '\'' +
+                ", operator=" + operator +
+                '}';
+    }
+
 }
