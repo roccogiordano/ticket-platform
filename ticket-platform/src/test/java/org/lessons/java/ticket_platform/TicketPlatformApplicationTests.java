e7ea4300e38ebf1750d2588594effdf9b717b57a
roccogiordano <rocco.giordano573@gmail.com>
2025-03-25 11:44:18 +0100

    feat: added spring project scaffholding

---
+++ ticket-platform/src/test/java/org/lessons/java/ticket_platform/TicketPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package org.lessons.java.ticket_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
