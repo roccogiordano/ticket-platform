c9d41ae4765293333b8399e22ab9387df98d93fb
roccogiordano <rocco.giordano573@gmail.com>
2025-03-26 23:23:03 +0100

    feat: added SecurityConfiguration class for Spring Security Configuration

---
+++ ticket-platform/src/main/java/org/lessons/java/ticket_platform/security/SecurityConfiguration.java
@@ -0,0 +1,77 @@
+package org.lessons.java.ticket_platform.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfiguration {
+
+    @Bean
+    DatabaseUserDetailsService userDetailsService() {
+        return new DatabaseUserDetailsService();
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    @Bean
+    DaoAuthenticationProvider authenticationProvider() {
+
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+
+        authProvider.setUserDetailsService(userDetailsService());
+        authProvider.setPasswordEncoder(passwordEncoder());
+
+        return authProvider;
+    }
+
+    @Bean
+    @SuppressWarnings("removal")
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http.authorizeHttpRequests()
+
+                .requestMatchers("/css/**", "/js/**", "/images/**", "/webjars/**").permitAll()
+
+                .requestMatchers("/tickets/create", "/tickets/edit/**").hasAuthority("ADMIN")
+                .requestMatchers(HttpMethod.POST, "/tickets/**").hasAuthority("ADMIN")
+                .requestMatchers("/tickets/{id}/note", "/notes/edit/**").hasAuthority("ADMIN")
+                .requestMatchers(HttpMethod.POST, "/notes/**").hasAuthority("ADMIN")
+
+                .requestMatchers("/tickets", "/tickets/**", "/notes", "/notes/create")
+                .hasAnyAuthority("USER", "ADMIN")
+
+                .anyRequest().permitAll();
+
+
+                http.formLogin(form -> form
+                    .loginPage("/login")
+                    .loginProcessingUrl("/login")
+                    .defaultSuccessUrl("/tickets", true)
+                    .permitAll()
+                );
+    
+                http.logout(logout -> logout
+                    .logoutUrl("/logout")
+                    .logoutSuccessUrl("/login?logout")
+                    .invalidateHttpSession(true)
+                    .deleteCookies("JSESSIONID")
+                    .permitAll()
+                );
+    
+                http.exceptionHandling(exception -> exception
+                    .accessDeniedPage("/access-denied")
+                );
+
+        return http.build();
+    }
+
+}
