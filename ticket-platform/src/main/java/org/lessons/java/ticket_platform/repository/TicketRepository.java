e4bc6f81cead794a999146641b9855fbe340bc01
roccogiordano <rocco.giordano573@gmail.com>
2025-03-26 11:40:11 +0100

    feat: implemented findByNameContaining method in TicketRepository

--- ticket-platform/src/main/java/org/lessons/java/ticket_platform/repository/TicketRepository.java
+++ ticket-platform/src/main/java/org/lessons/java/ticket_platform/repository/TicketRepository.java
@@ -1,8 +1,12 @@
 package org.lessons.java.ticket_platform.repository;
 
+import java.util.List;
+
 import org.lessons.java.ticket_platform.model.Ticket;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TicketRepository extends JpaRepository<Ticket, Integer> {
+
+    public List<Ticket> findByNameContaining(String name);
 
 }
