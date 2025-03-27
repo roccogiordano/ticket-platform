a3dd666282f4d94c65fa5d7032898811bbaded2a
roccogiordano <rocco.giordano573@gmail.com>
2025-03-27 12:15:25 +0100

    feat: added findByUser method to OperatorRepository

--- ticket-platform/src/main/java/org/lessons/java/ticket_platform/repository/OperatorRepository.java
+++ ticket-platform/src/main/java/org/lessons/java/ticket_platform/repository/OperatorRepository.java
@@ -1,8 +1,13 @@
 package org.lessons.java.ticket_platform.repository;
 
+import java.util.Optional;
+
 import org.lessons.java.ticket_platform.model.Operator;
+import org.lessons.java.ticket_platform.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface OperatorRepository extends JpaRepository<Operator, Integer> {
+
+    Optional<Operator> findByUser(User user);
 
 }
